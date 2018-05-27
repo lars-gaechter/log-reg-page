5041c17aa19de6e14dec610ef0c9b1617e98ac38
Toolcrafting <lars.gaechter1998@gmail.com>
2018-05-28 01:16:29 +0200

    Update table_users.sql
    Updated sql

--- table_users.sql
+++ table_users.sql
@@ -1,12 +1,15 @@
+/*
+Creates a new database login system with a table users in which users will be saved.
+*/
 DROP DATABASE IF EXISTS loginsystem;
 CREATE DATABASE loginsystem;
 USE loginsystem;
-DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS users;
 CREATE TABLE users (
 	user_id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    user_first VARCHAR(256) NOT NULL,
-    user_last VARCHAR(256) NOT NULL,
-    user_email VARCHAR(256) NOT NULL,
-    user_uid VARCHAR(256) NOT NULL,
-    user_pwd VARCHAR(256) NOT NULL
+	user_first VARCHAR(256) NOT NULL,
+	user_last VARCHAR(256) NOT NULL,
+	user_email VARCHAR(256) NOT NULL,
+	user_uid VARCHAR(256) NOT NULL,
+	user_pwd VARCHAR(256) NOT NULL
 );
